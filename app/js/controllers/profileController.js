ba20c5ff324c86e9a9930eeb12cdd78033160eec
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-06-08 23:09:32 +0200

    Improved profile modal like

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-  .controller('ProfileController', function($scope, $state, CurrentUser, RobotFactory) {
+  .controller('ProfileController', function($scope, CurrentUser, RobotFactory) {
     $scope.user = CurrentUser.user();
 
     $scope.robotList = RobotFactory;
@@ -14,47 +14,16 @@ angular.module('app')
     };
 
 
-    // MODAL CHAT //
+    // MODAL CHAT
+    $scope.chats = [];
+    $scope.query = "";
 
-    // var name = "Lya";
-    //
-    // $scope.chatting = function () {
-    //   var Ryo = 'Ryo'; //a variable that the computer can use as it's name
-    //   var chat = document.getElementById('chat').value; // used to get the text of user's input
-    //   // var chatuse = chat.toUpperCase(); //helps me easily identify what the user wrote even if they write with in lower or upper case.
-    //   $scope.query = '';
-    //
-    //   //this area will show all the possible responses to questions the user asks
-    //   if ($scope.query == 'Bonjour' || $scope.query == 'Salut') {
-    //     $scope.query = '';
-    //     document.getElementById('chatarea').innerHTML += '<span class="blue">' + name + '</span>: ' + chat + '<br />';
-    //     setTimeout(function() {
-    //       document.getElementById('chatarea').innerHTML += '<span class="red">' + Ryo + '</span>: Bonjour' + name + ', Comment vas-tu ? <br />';
-    //     }, 2000);
-    //   }
+    $scope.addChat = function () {
+      var value = $scope.query;
+      $scope.chats.push(value);
+      $scope.query = '';
+    };
 
-
-
-      // else if (chatuse.includes('BIEN') || chatuse.includes('BIEN')) {
-      //   document.getElementById('chat').value = '';
-      //   document.getElementById('chatarea').innerHTML += '<span class="blue">' + name + '</span>: ' + chat + '<br />';
-      //   setTimeout(function() {
-      //     document.getElementById('chatarea').innerHTML += '<span class="red">' + Ryo + '</span>: Super ! Et... que souhaites tu ? <br />';
-      //   }, 2000);
-      // } else if (chatuse.includes('VOIR') || chatuse.includes('RENCONTRER')) {
-      //   document.getElementById('chat').value = '';
-      //   document.getElementById('chatarea').innerHTML += '<span class="blue">' + name + '</span>: ' + chat + '<br />';
-      //   setTimeout(function() {
-      //     document.getElementById('chatarea').innerHTML += '<span class="red">' + Ryo + '</span>: Vraiment, cool...ce serait génial ! <br />';
-      //   }, 2000);
-      // } else if (chatuse.includes('RECHERCHE') || chatuse.includes('CYBORG')) {
-      //   document.getElementById('chat').value = '';
-      //   document.getElementById('chatarea').innerHTML += '<span class="blue">' + name + '</span>: ' + chat + '<br />';
-      //   setTimeout(function() {
-      //     document.getElementById('chatarea').innerHTML += '<span class="red">' + Ryo + '</span>: Je suis aussi fort que MARTIN ! <br />';
-      //   }, 2000);
-      // }
-    // };
-
+    console.log($scope.chats);
 
   });
